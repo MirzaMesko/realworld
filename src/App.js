0e17f6d97c83dcdc7825679843d2d89b8faa61f9
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-02-17 22:34:48 +0100

    add Route for Register component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
+import Register from './components/Register';
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Switch>
         <Route path="/login" exact component={Login} />
+        <Route path='/register' component={Register}/>
         <Route path="/" exact component={Home} />
       </Switch>
       <Footer />
