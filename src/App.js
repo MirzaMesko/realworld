93696267517f82753c11b45065f7be3b31802d5d
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-02-17 22:16:32 +0100

    add routes for Home and Login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import Header from './components/Header';
-import Home from './components/Home';
-import Footer from './components/Footer';
-import './App.css';
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Switch>
+        <Route path="/login" exact component={Login} />
+        <Route path="/" exact component={Home} />
+      </Switch>
       <Footer />
     </div>
   );
