762842f9fbd447ccf51acafd64aea22cac7ea4bf
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-02-17 22:35:24 +0100

    change href for Sign up to '/register'

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ function header() {
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/login">Sign up</a>
+            <a class="nav-link" href="/register">Sign up</a>
           </li>
         </ul>
       </div>
