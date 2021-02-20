4410f54d38bd7abce37346ffc7ccc59b16cf43a9
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-02-20 16:36:02 +0100

    connect to store and add log in functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,56 @@
-function login() {
-    return (
-        <div class="auth-page">
-  <div class="container page">
-    <div class="row">
+import { connect } from 'react-redux';
+import { login } from '../actions/users';
+import React, { useState } from 'react';
+import { NavLink as RouterLink } from 'react-router-dom';
 
-      <div class="col-md-6 offset-md-3 col-xs-12">
-        <h1 class="text-xs-center">Sign in</h1>
-        <p class="text-xs-center">
-          <a href="/register">Need an account?</a>
+function Login(props) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const { onLogin, token, user, history } = props;
+  console.log({user, token, user});
+
+  const login = (event) => {
+    event.preventDefault();
+    const user = {
+      email: email, 
+      password: password
+    }
+    onLogin(user).then(() => {
+      history.push('/');
+    });
+  }  
+    return (
+        <div className="auth-page">
+  <div className="container page">
+    <div className="row">
+
+      <div className="col-md-6 offset-md-3 col-xs-12">
+        <h1 className="text-xs-center">Sign in</h1>
+        <p className="text-xs-center">
+          <RouterLink to="/register">Need an account?</RouterLink>
         </p>
 
-        <ul class="error-messages">
+        <ul className="error-messages">
           <li>That email is already taken</li>
         </ul>
 
-        <form>
-          <fieldset class="form-group">
-            <input class="form-control form-control-lg" type="text" placeholder="Email" />
+        <form onSubmit={login}>
+          <fieldset className="form-group">
+            <input className="form-control form-control-lg" type="text" placeholder="Email" onChange={handleEmailChange} />
           </fieldset>
-          <fieldset class="form-group">
-            <input class="form-control form-control-lg" type="password" placeholder="Password" />
+          <fieldset className="form-group">
+            <input className="form-control form-control-lg" type="password" placeholder="Password" onChange={handlePasswordChange} />
           </fieldset>
-          <button class="btn btn-lg btn-primary pull-xs-right">
+          <button className="btn btn-lg btn-primary pull-xs-right">
             Sign in
           </button>
         </form>
@@ -33,4 +62,16 @@ function login() {
     )
 }
 
-export default login;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  token: state.token
+  }
+  
+}
+
+const mapDispatchToProps = dispatch => ({
+  onLogin: (user) => dispatch(login(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
