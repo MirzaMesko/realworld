6152b635121e3bd80ee83fac531963b03f2b7f4a
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-02-20 17:13:28 +0100

    connect to store and add autoSignup/logout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import React from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
 import Register from './components/Register';
-import "./App.css";
+import { logout, authCheckState } from './actions/users';
 
-function App() {
+function App(props) {
+  const { token, onLogout, onTryAutoSignup } = props;
+
+  React.useEffect(() => {
+    onTryAutoSignup();
+  }, [token]);
   return (
-    <div className="App">
-      <Header />
+    <div>
+      <Header token={token} onLogout={onLogout} />
       <Switch>
-        <Route path="/login" exact component={Login} />
-        <Route path='/register' component={Register}/>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
         <Route path="/" exact component={Home} />
       </Switch>
       <Footer />
@@ -20,4 +27,13 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  token: state.token,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onLogout: () => dispatch(logout()),
+  onTryAutoSignup: () => dispatch(authCheckState()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
