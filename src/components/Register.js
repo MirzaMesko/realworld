52d941fed2546594430a4af26cfa8d1b2bb2c3d8
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-02-20 16:37:55 +0100

    connect to store, add register user functionality

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,30 +1,65 @@
-function register() {
-    return (
-        <div class="auth-page">
-  <div class="container page">
-    <div class="row">
+import { connect } from 'react-redux';
+import { registerUser } from '../actions/users';
+import React, { useState } from 'react';
+import { NavLink as RouterLink } from 'react-router-dom';
 
-      <div class="col-md-6 offset-md-3 col-xs-12">
-        <h1 class="text-xs-center">Sign up</h1>
-        <p class="text-xs-center">
-          <a href="/login">Have an account?</a>
+function Register(props) {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    console.log(event.target.value);
+    setEmail(event.target.value);
+  };
+
+  const {currUser, token, onRegister} = props;
+  console.log({currUser, token});
+
+  const registerNewUser = (event) => {
+    event.preventDefault();
+    const user = {
+      username: username, 
+      email: email, 
+      password: password
+    }
+    console.log({user, currUser, token});
+    onRegister(user);
+  }  
+    return (
+        <div className="auth-page">
+  <div className="container page">
+    <div className="row">
+
+      <div className="col-md-6 offset-md-3 col-xs-12">
+        <h1 className="text-xs-center">Sign up</h1>
+        <p className="text-xs-center">
+          <RouterLink to="/login">Have an account?</RouterLink>
         </p>
 
-        <ul class="error-messages">
+        <ul className="error-messages">
           <li>That email is already taken</li>
         </ul>
 
-        <form>
-          <fieldset class="form-group">
-            <input class="form-control form-control-lg" type="text" placeholder="Username" />
+        <form onSubmit={registerNewUser}>
+          <fieldset className="form-group">
+            <input className="form-control form-control-lg" type="text" placeholder="Username" onChange={handleUsernameChange}/>
           </fieldset>
-          <fieldset class="form-group">
-            <input class="form-control form-control-lg" type="text" placeholder="Email" />
+          <fieldset className="form-group">
+            <input className="form-control form-control-lg" type="text" placeholder="Email" onChange={handleEmailChange}/>
           </fieldset>
-          <fieldset class="form-group">
-            <input class="form-control form-control-lg" type="password" placeholder="Password" />
+          <fieldset className="form-group">
+            <input className="form-control form-control-lg" type="password" placeholder="Password" onChange={handlePasswordChange}/>
           </fieldset>
-          <button class="btn btn-lg btn-primary pull-xs-right">
+          <button className="btn btn-lg btn-primary pull-xs-right" >
             Sign up
           </button>
         </form>
@@ -36,4 +71,16 @@ function register() {
     )
 }
 
-export default register;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  token: state.token
+  }
+  
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  onRegister: (user) => dispatch(registerUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
