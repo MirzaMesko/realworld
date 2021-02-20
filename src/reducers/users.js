5a243697fb1034364d4235175a632f2a3d3cc11f
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-02-20 16:42:56 +0100

    add returns for new user and login success

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,9 +1,26 @@
+import { REGISTER_NEW_USER } from '../actions/users';
+import { LOGIN_SUCCESS } from '../actions/users';
+
 const initialState = {
     token: '',
     user: {}
 }
 
 const reducer = (state = initialState, action) => {
+    if (action.type === REGISTER_NEW_USER) {
+        return {
+          ...state,
+          token: action.token,
+          user: action.user,
+        };
+      }
+      if (action.type === LOGIN_SUCCESS) {
+        return {
+          ...state,
+          token: action.token,
+          user: action.user,
+        };
+      }
     return state;
 }
 
