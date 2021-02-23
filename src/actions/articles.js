fbd330cb068bf2571396e7f83422974069823ad7
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-02-23 20:04:53 +0100

    add request func for tags & param for getArticles

--- src/actions/articles.js
+++ src/actions/articles.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 export const GET_ARTICLES = 'GET_ARTICLES';
+export const GET_TAGS = 'GET_TAGS';
 
 function getArticlesSuccess(articles) {
     return {
@@ -8,15 +9,42 @@ function getArticlesSuccess(articles) {
     }
 };
 
-export function getArticles() {
+function getTagsSuccess(tags) {
+  return {
+      type: GET_TAGS,
+      tags
+  }
+};
+
+
+
+export function getArticles(param) {
+  let url = `https://conduit.productionready.io/api/articles${param}`;
+  if (!param) {
+    url = `https://conduit.productionready.io/api/articles`
+  }
     return (dispatch) =>
       axios
-        .get('https://conduit.productionready.io/api/articles')
+        .get(url)
         .then((response) => {
             console.log(response.data.articles)
           dispatch(getArticlesSuccess(response.data.articles));
         })
         .catch((error) => {
-          console.log(error.errors);
+          console.log(error);
+        });
+}
+
+export function getTags() {
+    let url = `https://conduit.productionready.io/api/tags`;
+    return (dispatch) =>
+      axios
+        .get(url)
+        .then((response) => {
+            console.log(response.data.tags)
+          dispatch(getTagsSuccess(response.data.tags));
+        })
+        .catch((error) => {
+          console.log(error);
         });
 }
