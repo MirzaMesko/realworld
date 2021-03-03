aa5830b2e8e4857574cb2bc611caa5751703181d
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-03-03 18:35:39 +0100

    add functionality for followUser & favoriteArticle

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,111 +1,155 @@
 import React, { useState } from "react";
+import { NavLink as RouterLink } from "react-router-dom";
 import { connect } from "react-redux";
-import { getComments } from '../actions/articles'; 
+import { getComments, addComment, favoriteArticle, getArticles } from "../actions/articles";
+import { followUser } from '../actions/users';
 
 function SingleArticle(props) {
-    const { articles, match, onGetComments, token, comments } = props;
+  const [body, setBody] = useState("");
+  const {
+    articles,
+    match,
+    onGetComments,
+    token,
+    comments,
+    onAddComment,
+    onFavoriteArticle,
+    onGetArticle,
+    onFollowUser
+  } = props;
 
-    let article = articles.filter(item => `:${item.slug}` == match.params.id);
-    console.log(match.params.id, 'articles: ', articles, article);
+  const handleBodyChange = (event) => {
+    setBody(event.target.value);
+  };
 
-    React.useEffect(() => {
-        onGetComments(token, match.params.id.slice(1))
-    }, [match.params.id])
-    return (
-        <div className="article-page">
+  let article = articles;
+  if (articles.length > 1) {
+    article = articles.filter((item) => `:${item.slug}` == match.params.id);
+  }
 
-        <div className="banner">
-          <div className="container">
-            
-            <h1>{article[0].title}</h1>
-      
-            <div className="article-meta">
-              <a href=""><img src={article[0].author.image} /></a>
-              <div className="info">
-                <a href="" className="author">{article[0].author.username}</a>
-                <span className="date">{new Date(article[0].createdAt).toDateString()}</span>
-              </div>
-              <button className="btn btn-sm btn-outline-secondary">
-                <i className="ion-plus-round"></i>
-                &nbsp;
-                Follow {article[0].author.username} <span className="counter">(10)</span>
-              </button>
-              &nbsp;&nbsp;
-              <button className="btn btn-sm btn-outline-primary">
-                <i className="ion-heart"></i>
-                &nbsp;
-                Favorite Post <span className="counter">({article[0].favoritesCount})</span>
-              </button>
+  const comment = (event) => {
+    event.preventDefault();
+    onAddComment(token, match.params.id.slice(1), body);
+    event.target.reset();
+  };
+
+  React.useEffect(() => {
+    onGetComments(token, match.params.id.slice(1));
+  }, [match.params.id]);
+  return (
+    <div className="article-page">
+      <div className="banner">
+        <div className="container">
+          <h1>{article[0].title}</h1>
+
+          <div className="article-meta">
+            <a href="">
+              <img src={article[0].author.image} />
+            </a>
+            <div className="info">
+            <RouterLink to={`/@:${article[0].author.username}`} className="author" onClick={() => onGetArticle(`/?author=${article[0].author.username}`)}>
+                {article[0].author.username}
+              </RouterLink>
+              <span className="date">
+                {new Date(article[0].createdAt).toDateString()}
+              </span>
             </div>
-      
+            <button className="btn btn-sm btn-outline-secondary" onClick={() => onFollowUser(token, article[0].author.username)}>
+              <i className="ion-plus-round"></i>
+              &nbsp; {!article[0].author.following ? 'Follow' : 'Unfollow'} {article[0].author.username}{" "}
+              <span className="counter">(10)</span>
+            </button>
+            &nbsp;&nbsp;
+            <button className="btn btn-sm btn-outline-primary" onClick={() => onFavoriteArticle(token, article[0].slug)}>
+              <i className="ion-heart"></i>
+              &nbsp; {!article[0].favorited ? 'Favorite Post ' : 'Unfavorite Post '}
+              <span className="counter">({article[0].favoritesCount})</span>
+            </button>
           </div>
         </div>
-      
-        <div className="container page">
-      
-          <div className="row article-content">
-            <div className="col-md-12">
-              <p>
-              {article[0].description}
-              </p>
-              <h2 id="introducing-ionic">{article[0].title}</h2>
+      </div>
+
+      <div className="container page">
+        <div className="row article-content">
+          <div className="col-md-12">
+            <div>
               <p>{article[0].body}</p>
             </div>
+            {article[0].tagList.length > 1 && (
+              <ul className="tag-list">
+                {article[0].tagList.map((tag) => {
+                    return <li class="tag-default tag-pill tag-outline">{tag}</li>
+                })}
+              </ul>
+            )}
           </div>
-          <hr />
-      
-          <div className="row">
-      
-            <div className="col-xs-12 col-md-8 offset-md-2">
-      
-              <form className="card comment-form">
-                <div className="card-block">
-                  <textarea className="form-control" placeholder="Write a comment..." rows="3"></textarea>
-                </div>
-                <div className="card-footer">
-                  <img src={article[0].author.image} className="comment-author-img" />
-                  <button className="btn btn-sm btn-primary">
-                   Post Comment
-                  </button>
-                </div>
-              </form>
+        </div>
+        <hr />
 
-              {comments && comments.map(comment => {
-                  return (
-                    <div className="card" key={comment.id}>
+        <div className="row">
+          <div className="col-xs-12 col-md-8 offset-md-2">
+            <form className="card comment-form" onSubmit={comment}>
+              <div className="card-block">
+                <textarea
+                  className="form-control"
+                  placeholder="Write a comment..."
+                  rows="3"
+                  onChange={handleBodyChange}
+                ></textarea>
+              </div>
+              <div className="card-footer">
+                <img
+                  src={article[0].author.image}
+                  className="comment-author-img"
+                />
+                <button className="btn btn-sm btn-primary">Post Comment</button>
+              </div>
+            </form>
+
+            {comments && comments.map((comment) => {
+                let date = new Date(comment.createdAt);
+                return (
+                  <div className="card" key={comment.id}>
                     <div className="card-block">
                       <p className="card-text">{comment.body}</p>
                     </div>
                     <div className="card-footer">
                       <a href="" className="comment-author">
-                        <img src={comment.author.image} className="comment-author-img" />
+                        <img
+                          src={comment.author.image}
+                          className="comment-author-img"
+                        />
                       </a>
                       &nbsp;
-                      <a href="" className="comment-author">{comment.author.username}</a>
-                      <span className="date-posted">{new Date(comment.createdAt).toDateString()}</span>
+                      <a href="" className="comment-author">
+                        {comment.author.username}
+                      </a>
+                      <span className="date-posted">
+                        {date.toDateString()}, {date.getHours()}:{date.getMinutes()}
+                      </span>
                     </div>
                   </div>
-                  )
+                );
               })}
-              
-            </div>
-      
           </div>
-      
         </div>
-      
       </div>
-    )
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => ({
-    articles: state.articles.articles,
-    token: state.users.token,
-    comments: state.articles.comments
-  });
+  articles: state.articles.articles,
+  token: state.users.token,
+  comments: state.articles.comments,
+});
 
-  const mapDispatchToProps = dispatch => ({
-    onGetComments: (token, slug) => dispatch(getComments(token, slug))
-})
+const mapDispatchToProps = (dispatch) => ({
+  onGetComments: (token, slug) => dispatch(getComments(token, slug)),
+  onAddComment: (token, slug, body) => dispatch(addComment(token, slug, body)),
+  onFavoriteArticle: (token, slug) => dispatch(favoriteArticle(token, slug)),
+  onGetArticle: (param) => dispatch(getArticles(param)),
+  onFollowUser: (token, username) => dispatch(followUser(token, username))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleArticle);
