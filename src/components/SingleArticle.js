b9f7d62928b7acd7cdbdb9c0e67b87e14c24ce2f
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-03-06 14:34:33 +0100

    import and implement followUser()/unfollowUser()

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import { NavLink as RouterLink } from "react-router-dom";
 import { connect } from "react-redux";
-import { getComments, addComment, favoriteArticle, getArticles, unfavoriteArticle } from "../actions/articles";
-import { followUser } from '../actions/users';
+import {
+  getComments,
+  addComment,
+  favoriteArticle,
+  getArticles,
+  unfavoriteArticle,
+} from "../actions/articles";
+import { followUser, unfollowUser } from "../actions/users";
 
 function SingleArticle(props) {
   const [body, setBody] = useState("");
   const {
     articles,
     match,
+    user,
     onGetComments,
     token,
     comments,
@@ -16,7 +23,9 @@ function SingleArticle(props) {
     onFavoriteArticle,
     onGetArticle,
     onFollowUser,
-    onUnfavoriteArticle
+    onUnfavoriteArticle,
+    onUnfollowUser,
+    profile
   } = props;
 
   const handleBodyChange = (event) => {
@@ -34,6 +43,72 @@ function SingleArticle(props) {
     event.target.reset();
   };
 
+  let buttons = (
+    <span>
+      {profile.following === false ? (
+        <button
+          className="btn btn-sm btn-outline-secondary"
+          onClick={() => onFollowUser(token, profile.username)}
+        >
+          <i className="ion-plus-round"></i>
+          &nbsp; Follow {profile.username}
+          <span className="counter">(10)</span>
+        </button>
+      ) : (
+        <button
+          className="btn btn-sm btn-outline-secondary"
+          onClick={() => onUnfollowUser(token, profile.username)}
+        >
+          <i className="ion-plus-round"></i>
+          &nbsp; Unfollow {profile.username}
+          <span className="counter">(10)</span>
+        </button>
+      )}
+      &nbsp;&nbsp;
+      {!article[0].favorited ? (
+        <button
+          className="btn btn-sm btn-outline-primary"
+          onClick={() => onFavoriteArticle(token, article[0].slug)}
+        >
+          <i className="ion-heart"></i>
+          &nbsp; Favorite Post
+          <span className="counter">({article[0].favoritesCount})</span>
+        </button>
+      ) : (
+        <button
+          className="btn btn-sm btn-outline-primary"
+          onClick={() => onUnfavoriteArticle(token, article[0].slug)}
+        >
+          <i className="ion-heart"></i>
+          &nbsp; Unfavorite Post
+          <span className="counter">({article[0].favoritesCount})</span>
+        </button>
+      )}
+    </span>
+  );
+
+  if (profile.username === user) {
+    buttons = (
+      <span>
+        <button
+          className="btn btn-sm btn-outline-secondary"
+          onClick={() => onFavoriteArticle(token, article[0].slug)}
+        >
+          <i className="ion-edit"></i>
+          &nbsp; Edit Article
+        </button>
+        &nbsp;&nbsp;
+        <button
+          className="btn btn-sm btn-outline-danger"
+          onClick={() => onFavoriteArticle(token, article[0].slug)}
+        >
+          <i className="ion-trash-a"></i>
+          &nbsp; Delete Article
+        </button>
+      </span>
+    );
+  }
+
   React.useEffect(() => {
     onGetComments(token, match.params.id.slice(1));
   }, [match.params.id]);
@@ -45,34 +120,23 @@ function SingleArticle(props) {
 
           <div className="article-meta">
             <a href="">
-              <img src={article[0].author.image} />
+              <img src={profile.image} />
             </a>
             <div className="info">
-            <RouterLink to={`/@:${article[0].author.username}`} className="author" onClick={() => onGetArticle(`/?author=${article[0].author.username}`)}>
-                {article[0].author.username}
+              <RouterLink
+                to={`/@:${profile.username}`}
+                className="author"
+                onClick={() =>
+                  onGetArticle(`/?author=${profile.username}`)
+                }
+              >
+                {profile.username}
               </RouterLink>
               <span className="date">
                 {new Date(article[0].createdAt).toDateString()}
               </span>
             </div>
-            <button className="btn btn-sm btn-outline-secondary" onClick={() => onFollowUser(token, article[0].author.username)}>
-              <i className="ion-plus-round"></i>
-              &nbsp; {!article[0].author.following ? 'Follow' : 'Unfollow'} {article[0].author.username}{" "}
-              <span className="counter">(10)</span>
-            </button>
-            &nbsp;&nbsp;
-            {!article[0].favorited ? 
-            <button className="btn btn-sm btn-outline-primary" onClick={() => onFavoriteArticle(token, article[0].slug)}>
-              <i className="ion-heart"></i>
-              &nbsp; Favorite Post 
-              <span className="counter">({article[0].favoritesCount})</span>
-            </button>
-            : 
-            <button className="btn btn-sm btn-outline-primary" onClick={() => onUnfavoriteArticle(token, article[0].slug)}>
-              <i className="ion-heart"></i>
-              &nbsp; Unfavorite Post 
-              <span className="counter">({article[0].favoritesCount})</span>
-            </button>}
+            {buttons}
           </div>
         </div>
       </div>
@@ -86,7 +150,9 @@ function SingleArticle(props) {
             {article[0].tagList.length > 1 && (
               <ul className="tag-list">
                 {article[0].tagList.map((tag) => {
-                    return <li class="tag-default tag-pill tag-outline">{tag}</li>
+                  return (
+                    <li class="tag-default tag-pill tag-outline">{tag}</li>
+                  );
                 })}
               </ul>
             )}
@@ -107,14 +173,15 @@ function SingleArticle(props) {
               </div>
               <div className="card-footer">
                 <img
-                  src={article[0].author.image}
+                  src={profile.image}
                   className="comment-author-img"
                 />
                 <button className="btn btn-sm btn-primary">Post Comment</button>
               </div>
             </form>
 
-            {comments && comments.map((comment) => {
+            {comments &&
+              comments.map((comment) => {
                 let date = new Date(comment.createdAt);
                 return (
                   <div className="card" key={comment.id}>
@@ -133,7 +200,8 @@ function SingleArticle(props) {
                         {comment.author.username}
                       </a>
                       <span className="date-posted">
-                        {date.toDateString()}, {date.getHours()}:{date.getMinutes()}
+                        {date.toDateString()}, {date.getHours()}:
+                        {date.getMinutes()}
                       </span>
                     </div>
                   </div>
@@ -150,15 +218,19 @@ const mapStateToProps = (state) => ({
   articles: state.articles.articles,
   token: state.users.token,
   comments: state.articles.comments,
+  user: state.users.user,
+  profile: state.users.profile
 });
 
 const mapDispatchToProps = (dispatch) => ({
   onGetComments: (token, slug) => dispatch(getComments(token, slug)),
   onAddComment: (token, slug, body) => dispatch(addComment(token, slug, body)),
   onFavoriteArticle: (token, slug) => dispatch(favoriteArticle(token, slug)),
-  onUnfavoriteArticle: (token, slug) => dispatch(unfavoriteArticle(token, slug)),
+  onUnfavoriteArticle: (token, slug) =>
+    dispatch(unfavoriteArticle(token, slug)),
   onGetArticle: (param) => dispatch(getArticles(param)),
-  onFollowUser: (token, username) => dispatch(followUser(token, username))
+  onFollowUser: (token, username) => dispatch(followUser(token, username)),
+  onUnfollowUser: (token, username) => dispatch(unfollowUser(token, username)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleArticle);
