28a25952a787de122a66f407c6cb1709889edb93
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-03-06 14:26:37 +0100

    add getProfile() to onClick in link to user page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink as RouterLink } from 'react-router-dom';
 
 function Header(props) {
-  const { token, user, onGetArticle } = props;
+  const { token, user, onGetArticle, onGetProfile } = props;
     return (
         <nav className="navbar navbar-light">
       <div className="container">
@@ -23,7 +23,7 @@ function Header(props) {
           </RouterLink>
         </li>
         <li className="nav-item">
-          <RouterLink to={`/@:${user}`} className="nav-link" onClick={() => onGetArticle(`/?author=${user}`)}>
+          <RouterLink to={`/@:${user}`} className="nav-link" onClick={() => onGetArticle(`/?author=${user}`).then(() => onGetProfile(user))}>
             {user}
             </RouterLink>
         </li>
