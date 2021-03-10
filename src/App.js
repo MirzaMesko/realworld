cdca7cd81156f9fc372c7b086311f71f46b5dad8
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-03-10 19:49:58 +0100

    add route for editing an article

--- src/App.js
+++ src/App.js
@@ -5,35 +5,56 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
-import Register from './components/Register';
-import SingleArticle from './components/SingleArticle';
-import Profile from './components/Profile';
-import Settings from './components/Settings';
-import CreateArticle from './components/CreateArticle';
-import { logout, authCheckState, getProfile } from './actions/users';
-import { getArticles, getTags } from './actions/articles';
+import Register from "./components/Register";
+import SingleArticle from "./components/SingleArticle";
+import Profile from "./components/Profile";
+import Settings from "./components/Settings";
+import CreateArticle from "./components/CreateArticle";
+import {
+  logout,
+  authCheckState,
+  getProfile,
+  getCurrentUser,
+} from "./actions/users";
+import { getArticles, getTags } from "./actions/articles";
 
 function App(props) {
-  const { token, onLogout, onTryAutoSignup, onGetArticles, onGetTags, user, onGetProfile } = props;
+  const {
+    token,
+    onLogout,
+    onTryAutoSignup,
+    onGetArticles,
+    onGetTags,
+    user,
+    onGetProfile,
+    history,
+    onGetCurrentUser,
+    currentUser,
+  } = props;
+  
 
   React.useEffect(() => {
     onTryAutoSignup();
     onGetArticles();
     onGetTags();
+    onGetCurrentUser(token);
   }, [token]);
   return (
     <div>
-      <Header token={token} onLogout={onLogout} user={user} onGetArticle={onGetArticles} onGetProfile={onGetProfile} />
+      <Header
+        onLogout={onLogout}
+        onGetArticle={onGetArticles}
+        onGetProfile={onGetProfile}
+      />
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route exact path="/@:id" component={Profile} />
         <Route exact path="/@:id/favorited" component={Profile} />
         <Route path="/article/:id" component={SingleArticle} />
-        <Route path="/editor" component={CreateArticle} />
-        <Route path="/settings" >
-          <Settings onLogout={onLogout} token={token}/>
-        </Route>
+        <Route exact path="/editor" component={CreateArticle} />
+        <Route path="/editor/:id" component={CreateArticle} />
+        <Route path="/settings" render={() => <Settings onLogout={onLogout} token={token} onGetArticle={onGetArticles} {...props}/>} />
         <Route path="/yourFeed" component={Home} />
         <Route exact path="/" component={Home} />
       </Switch>
@@ -45,7 +66,8 @@ function App(props) {
 const mapStateToProps = (state) => ({
   token: state.users.token,
   articles: state.users.articles,
-  user: state.users.user
+  user: state.users.user,
+  currentUser: state.users.currentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -53,7 +75,8 @@ const mapDispatchToProps = (dispatch) => ({
   onTryAutoSignup: () => dispatch(authCheckState()),
   onGetArticles: (param) => dispatch(getArticles(param)),
   onGetTags: () => dispatch(getTags()),
-  onGetProfile: (username) => dispatch(getProfile(username))
+  onGetProfile: (username) => dispatch(getProfile(username)),
+  onGetCurrentUser: (token) => dispatch(getCurrentUser(token)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
