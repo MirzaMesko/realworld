894868a239c8b711b4d1e3cbe2b18c749650be30
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-03-10 20:03:05 +0100

    enable display of errors if registration fails

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,13 @@
-import { connect } from 'react-redux';
-import { registerUser } from '../actions/users';
-import React, { useState } from 'react';
-import { NavLink as RouterLink } from 'react-router-dom';
+import { connect } from "react-redux";
+import { registerUser } from "../actions/users";
+import React, { useState } from "react";
+import { NavLink as RouterLink } from "react-router-dom";
 
 function Register(props) {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
   const [email, setEmail] = useState();
+  const [errors, setErrors] = useState([]);
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -17,67 +18,92 @@ function Register(props) {
   };
 
   const handleEmailChange = (event) => {
-    console.log(event.target.value);
     setEmail(event.target.value);
   };
 
-  const {currUser, token, onRegister} = props;
-  console.log({currUser, token});
+  const { currUser, token, onRegister, history } = props;
 
   const registerNewUser = (event) => {
     event.preventDefault();
     const user = {
-      username: username, 
-      email: email, 
-      password: password
-    }
-    console.log({user, currUser, token});
-    onRegister(user);
-  }  
-    return (
-        <div className="auth-page">
-  <div className="container page">
-    <div className="row">
+      username: username,
+      email: email,
+      password: password,
+    };
+    onRegister(user).then((response) => {
+      if (response.status === 200) {
+        history.push("/");
+      }
+      if (response.statusCode !== 200) {
+        let keys = Object.keys(response);
+        let error = [];
+        keys.map((key) => {
+          error.push([key + " " + response[key]]);
+        });
+        setErrors(error);
+      }
+    });
+  };
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign up</h1>
+            <p className="text-xs-center">
+              <RouterLink to="/login">Have an account?</RouterLink>
+            </p>
 
-      <div className="col-md-6 offset-md-3 col-xs-12">
-        <h1 className="text-xs-center">Sign up</h1>
-        <p className="text-xs-center">
-          <RouterLink to="/login">Have an account?</RouterLink>
-        </p>
+            {errors.length ? (
+              <ul className="error-messages">
+                {errors.map((error) => (
+                  <li key={error}>{error}</li>
+                ))}
+              </ul>
+            ) : null}
 
-        <ul className="error-messages">
-          <li>That email is already taken</li>
-        </ul>
-
-        <form onSubmit={registerNewUser}>
-          <fieldset className="form-group">
-            <input className="form-control form-control-lg" type="text" placeholder="Username" onChange={handleUsernameChange}/>
-          </fieldset>
-          <fieldset className="form-group">
-            <input className="form-control form-control-lg" type="text" placeholder="Email" onChange={handleEmailChange}/>
-          </fieldset>
-          <fieldset className="form-group">
-            <input className="form-control form-control-lg" type="password" placeholder="Password" onChange={handlePasswordChange}/>
-          </fieldset>
-          <button className="btn btn-lg btn-primary pull-xs-right" >
-            Sign up
-          </button>
-        </form>
+            <form onSubmit={registerNewUser}>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  placeholder="Username"
+                  onChange={handleUsernameChange}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  placeholder="Email"
+                  onChange={handleEmailChange}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="password"
+                  placeholder="Password"
+                  onChange={handlePasswordChange}
+                />
+              </fieldset>
+              <button className="btn btn-lg btn-primary pull-xs-right">
+                Sign up
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-
     </div>
-  </div>
-</div>
-    )
+  );
 }
 
 const mapStateToProps = (state) => {
   return {
     user: state.user,
-  token: state.token
-  }
-  
-}
+    token: state.token,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => ({
   onRegister: (user) => dispatch(registerUser(user)),
